737b767d7f0f01ff6def3a7bab98373b9256c806
Honsonstar <lhxjijijinqu@163.com>
2026-04-03 13:46:29 +0800

    0403

--- scripts/run_ablation_simple.sh
+++ scripts/run_ablation_simple.sh
@@ -213,7 +213,7 @@ run_mode() {
                 --type_of_path combine \
                 --max_epochs "$MAX_EPOCHS" \
                 --lr "$LR" \
-                --opt adam \
+                --opt adamW \
                 --reg "$REG" \
                 --alpha_surv 0.5 \
                 --weighted_sample \
